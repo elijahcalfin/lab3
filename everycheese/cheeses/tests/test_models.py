c2d189e1061109ab5c94d51986f0fd5cc0354156
Elijah Calfin <hhh510hhh510@gmail.com>
2021-03-10 20:13:57 -0500

    first commit

---
+++ everycheese/cheeses/tests/test_models.py
@@ -0,0 +1,11 @@
+import pytest
+
+#Connects our tests with our db
+pytestmark = pytest.mark.django_db
+from ..models import Cheese
+from .factories import CheeseFactory
+
+def test__str__():
+	cheese = CheeseFactory()
+	assert cheese.__str__() == cheese.name
+	assert str(cheese) == cheese.name
